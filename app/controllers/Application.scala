69ee4d13753af73c0eafa8b12f41d54644dcdf29
Ben McCann <ben@connectifier.com>
2014-10-23 17:29:14 -0700

    Handle a null script engine in a better manner than just throwing a NullPointerException

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -14,18 +14,21 @@ object Application extends Controller {
     //
     // See: https://github.com/playframework/playframework/issues/2532
     val engine = new ScriptEngineManager(null).getEngineByName("nashorn")
+    if (engine == null) {
+      BadRequest("Nashorn script engine not found. Are you using JDK 8?")
+    } else {
+      // React expects `window` or `global` to exist. Create a `global` pointing
+      // to Nashorn's context to give React a place to define its global namespace.
+      engine.eval("var global = this;")
 
-    // React expects `window` or `global` to exist. Create a `global` pointing
-    // to Nashorn's context to give React a place to define its global namespace.
-    engine.eval("var global = this;")
+      // Evaulate React and the application code.
+      engine.eval(new FileReader("public/javascripts/react-with-addons-0.10.0.js"))
+      engine.eval(new FileReader("public/javascripts/components/App.js"))
 
-    // Evaulate React and the application code.
-    engine.eval(new FileReader("public/javascripts/react-with-addons-0.10.0.js"))
-    engine.eval(new FileReader("public/javascripts/components/App.js"))
-
-    Ok(views.html.main("React on Play") {
-      templates.Html(engine.eval("React.renderComponentToString(App());").toString)
-    })
+      Ok(views.html.main("React on Play") {
+        templates.Html(engine.eval("React.renderComponentToString(App());").toString)
+      })
+    }
   }
 
 }
